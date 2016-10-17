e2854ca041fead68a81c80331b3aa874d1ce2690
DrewWarrenTIY <drewwarrentiy@gmail.com>
2016-10-17 15:30:58 -0600

    feat(init) Add JS, CSS, HTML, Link to API
    Used jQuery to access open.whitehouse.gov database.
    Confirmed that data is being pulled. It is as an
    array of objects.

---
+++ scripts/taxideas.js
@@ -0,0 +1,28 @@
+/* https://open.whitehouse.gov/ data from the
+   Tax Ideas Submitted by the Public
+*/
+
+var ourData = {};
+
+window.onload = function taxIdeas () {
+
+  let url = 'https://open.whitehouse.gov/resource/mj9q-chxc.json'
+
+  // over 27,000 hits with the following
+  // $.get(url, function(data) {
+  //   console.log(data);
+  // });
+
+  // we'll do the long version to prevent overload
+  $.ajax({
+    dataType: "json",
+    url: url,
+    data: {
+      "$limit" : 100
+    }
+  }).done(function(data) {
+     ourData = data;
+     console.log(data);
+  })
+
+};
