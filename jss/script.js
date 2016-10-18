eae8156f6aa181e71866ed66ed5a6732bf842a4b
DrewWarrenTIY <drewwarrentiy@gmail.com>
2016-10-17 23:28:22 -0600

    feat(init) Add 25 divs, one for each grouping of data
    Name, Org, OrgType all put together into one div, 25 times

--- jss/script.js
+++ jss/script.js
@@ -5,10 +5,14 @@ function taxIdeaData(data) {
   nameArray = [];
   orgArray = [];
   orgTypeArray = [];
+  eachArray = [];
   for (var i = 0; i < data.length; i++) {
   nameArray.push(data[i].name);
   orgArray.push(data[i].organization);
   orgTypeArray.push(data[i].type_of_organization);
+  eachArray[i] = nameArray[i] + ", " + orgArray [i] + ", " + orgTypeArray[i];
+
+  document.getElementById(i.toString()).innerHTML = eachArray[i].toString();
 
   document.querySelector('.dataTestOne').innerHTML = nameArray.join(" ");
   document.querySelector('.dataTestTwo').innerHTML = orgArray.join(" ");
@@ -17,5 +21,6 @@ function taxIdeaData(data) {
 console.log(nameArray);
 console.log(orgArray);
 console.log(orgTypeArray);
+console.log(eachArray);
 
 }
