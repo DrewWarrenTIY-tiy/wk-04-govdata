5299d02a28d782cc4e8d0e75b2c4cb0364b0d68e
DrewWarrenTIY <drewwarrentiy@gmail.com>
2016-10-17 18:58:45 -0600

    feat(init) Pulls names from API data into HTML

--- jss/script.js
+++ jss/script.js
@@ -1,3 +1,10 @@
 function taxIdeaData(data) {
+
+
   console.log(data);
+
+  document.querySelector('.dataTest').innerHTML = data[0].name + " " + data[1].name + "\n " + data[2].name + "\n " + data[3].name + "\n " + data[4].name + "\n " + data[5].name + "\n "
+  + data[6].name + "\n " + data[7].name + "\n " + data[8].name + "\n " + data[9].name + "\n " + data[10].name + "\n " + data[11].name + "\n " + data[12].name +
+  "\n " + data[13].name + "\n " + data[14].name + "\n " + data[15].name + "\n " + data[16].name + "\n " + data[17].name + "\n " + data[18].name + "\n "
+  + data[19].name + "\n " + data[20].name + "\n " + data[21].name + "\n " + data[22].name + "\n " + data[23].name + "\n " + data[24].name;
 }
