5264be6c39d007962269687b822ca4f398451582
DrewWarrenTIY <drewwarrentiy@gmail.com>
2016-10-17 22:25:50 -0600

    feat(init) Pulled 3 data types from data set.
    Still in arrays, joined to strings for display in HTML
    Need to break them up into groups by array index.

--- jss/script.js
+++ jss/script.js
@@ -2,9 +2,20 @@ function taxIdeaData(data) {
 
 
   console.log(data);
+  nameArray = [];
+  orgArray = [];
+  orgTypeArray = [];
+  for (var i = 0; i < data.length; i++) {
+  nameArray.push(data[i].name);
+  orgArray.push(data[i].organization);
+  orgTypeArray.push(data[i].type_of_organization);
 
-  document.querySelector('.dataTest').innerHTML = data[0].name + " " + data[1].name + "\n " + data[2].name + "\n " + data[3].name + "\n " + data[4].name + "\n " + data[5].name + "\n "
-  + data[6].name + "\n " + data[7].name + "\n " + data[8].name + "\n " + data[9].name + "\n " + data[10].name + "\n " + data[11].name + "\n " + data[12].name +
-  "\n " + data[13].name + "\n " + data[14].name + "\n " + data[15].name + "\n " + data[16].name + "\n " + data[17].name + "\n " + data[18].name + "\n "
-  + data[19].name + "\n " + data[20].name + "\n " + data[21].name + "\n " + data[22].name + "\n " + data[23].name + "\n " + data[24].name;
+  document.querySelector('.dataTestOne').innerHTML = nameArray.join(" ");
+  document.querySelector('.dataTestTwo').innerHTML = orgArray.join(" ");
+  document.querySelector('.dataTestThree').innerHTML = orgTypeArray.join(" ");
+}
+console.log(nameArray);
+console.log(orgArray);
+console.log(orgTypeArray);
+
 }
